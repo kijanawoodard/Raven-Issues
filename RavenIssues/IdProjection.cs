85981ad8f40df6563e3306fa60e8c8b23999bb8b
Kijana Woodard <github@wyldeye.com>
2012-06-02 08:41:15 -0500

    made sure tests run in order and made sure indexes are fresh
    added another passing test that verifies the count

--- RavenIssues/IdProjection.cs
+++ RavenIssues/IdProjection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Raven.Client;
 using Raven.Client.Embedded;
+using Raven.Client.Listeners;
 using Xunit;
 
 namespace RavenIssues
@@ -10,14 +11,16 @@ namespace RavenIssues
     public class ProjectionTests : IDisposable
     {
         protected DateTimeOffset Now { get; private set; }
-        protected IDocumentStore DocumentStore { get; private set; }
+        protected EmbeddableDocumentStore DocumentStore { get; private set; }
         protected IDocumentSession Session { get; private set; }
 
         public ProjectionTests()
         {
             Now = DateTimeOffset.Now;
 
-            DocumentStore = new EmbeddableDocumentStore { RunInMemory = true }.Initialize();
+            DocumentStore = new EmbeddableDocumentStore { RunInMemory = true };
+            DocumentStore.RegisterListener(new NoStaleQueriesAllowed());
+            DocumentStore.Initialize();
             Session = DocumentStore.OpenSession();
 
             Setup();
@@ -43,8 +46,45 @@ namespace RavenIssues
             DocumentStore.Dispose();
         }
 
+        //This works as expected
         [Fact]
-        public void ShouldBeAbleToProjectIdOntoAnotherName()
+        public void _0_ActuallyGetData()
+        {
+            var foos =
+                Session
+                    .Query<Foo>()
+                    .Where(foo => foo.Data > 1)
+                    .Select(foo => new FooWithId
+                    {
+                        Id = foo.Id,
+                        Data = foo.Data
+                    })
+                    .ToList();
+
+            Assert.True(foos.Count == 3);
+        }
+
+        //This works as expected
+        [Fact]
+        public void _1_ShouldBeAbleToProjectIdOntoAnotherFieldCalledId()
+        {
+            var foos =
+                Session
+                    .Query<Foo>()
+                    .Where(foo => foo.Data > 1)
+                    .Select(foo => new FooWithId
+                    {
+                        Id = foo.Id,
+                        Data = foo.Data
+                    })
+                    .ToList();
+
+            Xunit.Assert.NotNull(foos[0].Id);
+        }
+
+        //Fails
+        [Fact]
+        public void _2_ShouldBeAbleToProjectIdOntoAnotherName()
         {
             var foos =
                 Session
@@ -60,8 +100,9 @@ namespace RavenIssues
             Xunit.Assert.NotNull(foos[0].FooId);
         }
 
+        //Fails
         [Fact]
-        public void ShouldBeAbleToProjectIdOntoAnotherName_AndAnotherFieldNamedIdShouldNotBeAffected()
+        public void _3_ShouldBeAbleToProjectIdOntoAnotherName_AndAnotherFieldNamedIdShouldNotBeAffected()
         {
             var foos =
                 Session
@@ -76,24 +117,6 @@ namespace RavenIssues
 
             Xunit.Assert.Null(foos[0].Id);
             Xunit.Assert.NotNull(foos[0].FooId);
-        }
-
-        //This works as expected
-        [Fact]
-        public void ShouldBeAbleToProjectIdOntoAnotherFieldCalledId()
-        {
-            var foos =
-                Session
-                    .Query<Foo>()
-                    .Where(foo => foo.Data > 1)
-                    .Select(foo => new FooWithId
-                    {
-                        Id = foo.Id,
-                        Data = foo.Data
-                    })
-                    .ToList();
-
-            Xunit.Assert.NotNull(foos[0].Id);
         }
 
         private class Foo
@@ -119,6 +142,14 @@ namespace RavenIssues
             public string FooId { set; get; }
             public string Id { set; get; }
             public int Data { set; get; }
+        }
+    }
+
+    public class NoStaleQueriesAllowed : IDocumentQueryListener
+    {
+        public void BeforeQueryExecuted(IDocumentQueryCustomization queryCustomization)
+        {
+            queryCustomization.WaitForNonStaleResults();
         }
     }
 }
