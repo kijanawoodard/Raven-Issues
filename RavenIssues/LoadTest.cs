c09ad8ad5a5bdfb98581aaf359863c191013a674
Kijana Woodard <github@wyldeye.com>
2012-07-13 16:06:58 -0500

    test loadstartingWith

---
+++ RavenIssues/LoadTest.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using Xunit;
+
+namespace RavenIssues
+{
+    public class LoadTest : IDisposable
+    {
+        protected EmbeddableDocumentStore DocumentStore { get; private set; }
+        protected IDocumentSession Session { get; private set; }
+
+        public LoadTest()
+        {
+            DocumentStore = new EmbeddableDocumentStore { RunInMemory = true };
+            DocumentStore.RegisterListener(new NoStaleQueriesAllowed());
+            DocumentStore.Initialize();
+//            IndexCreation.CreateIndexes(typeof(BlankTestIndex).Assembly, DocumentStore);
+
+            Session = DocumentStore.OpenSession();
+
+            Setup();
+        }
+
+        private void Setup()
+        {
+            var foos =
+                Enumerable.Range(1, 1000)
+                    .Select(x => new Foo {Id = "tenant/1/foo/"})
+                    .ToList();
+
+            foos.ForEach(Session.Store);
+
+            Session.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Session.Dispose();
+            DocumentStore.Dispose();
+        }
+
+        public class Foo
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void SomeFactAboutFoo()
+        {
+            var foos =
+                Session
+                    .LoadStartingWith<Foo>("tenant/1", 0, 1000)
+                    .ToList();
+
+            Assert.Equal(1000, foos.Count);
+        }
+    }
+
+    public static class SessionExtensions
+    {
+        public static IEnumerable<T> LoadStartingWith<T>(this IDocumentSession session,
+                       string keyPrefix, int start = 0, int pageSize = 25)
+        {
+            var inMemorySession = session as InMemoryDocumentSessionOperations;
+            if (inMemorySession == null)
+            {
+                throw new InvalidOperationException(
+                    "LoadStartingWith(..) only works on InMemoryDocumentSessionOperations");
+            }
+
+            return session.Advanced.DatabaseCommands.StartsWith(keyPrefix, start, pageSize)
+                        .Select(inMemorySession.TrackEntity<T>)
+                        .ToList();
+        }
+    }
+}
